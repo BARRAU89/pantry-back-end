37018257ad241f31abf175a4544635efaafab51d
Raul Barrios <barrau89@gmail.com>
2023-01-21 21:18:44 -0600

    first commit

---
+++ src/main/java/net/yorksolutions/apantrybe/APantryBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.apantrybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APantryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APantryBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/apantrybe/APantryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.apantrybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APantryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
